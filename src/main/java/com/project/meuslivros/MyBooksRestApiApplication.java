aa95b0b52f5ada8b80c5865846cf675a3848ee08
Dikendev <diken.dev@gmail.com>
2023-04-12 02:01:42 -0300

    Initial commit

---
+++ src/main/java/com/project/meuslivros/MyBooksRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.meuslivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBooksRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBooksRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/meuslivros/MeusLivrosRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.meuslivros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusLivrosRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
