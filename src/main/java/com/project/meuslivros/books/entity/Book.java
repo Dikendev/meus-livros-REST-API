3306db119047edb2eeba6d555e43318629187996
Dikendev <diken.dev@gmail.com>
2023-04-12 03:02:59 -0300

    Book entity

---
+++ src/main/java/com/project/meuslivros/books/entity/Book.java
@@ -0,0 +1,43 @@
+package com.project.meuslivros.books.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,
+            generator = "UUID")
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+    private String subTitle;
+    @NotBlank(message = "Author is required")
+    private String author;
+    private String publishedBy;
+    private Date publishedDate;
+    private String edition;
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String bookImageUrl;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id")
+    private Category category;
+    private String createAt = new SimpleDateFormat(
+            "dd-MM-yyyy HH:mm:ss z")
+            .format(new Date());
+
+}
