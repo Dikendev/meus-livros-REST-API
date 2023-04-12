937f587c421f3324e2c13d1955d653af17c76223
Dikendev <diken.dev@gmail.com>
2023-04-12 08:41:33 -0300

    Category Repository

---
+++ src/main/java/com/project/meuslivros/books/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.project.meuslivros.books.repository;
+
+import com.project.meuslivros.books.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+}
