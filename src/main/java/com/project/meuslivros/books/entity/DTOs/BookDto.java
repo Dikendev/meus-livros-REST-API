a884920b8a40da856b40d2ac9efaf72ee1f8b640
Dikendev <diken.dev@gmail.com>
2023-04-13 04:38:45 -0300

    Book DTO

---
+++ src/main/java/com/project/meuslivros/books/entity/DTOs/BookDto.java
@@ -0,0 +1,23 @@
+package com.project.meuslivros.books.entity.DTOs;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+
+    private UUID Id;
+    private UUID categoryId;
+    @JoinColumn(name = "language_id")
+    private UUID LanguageId;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+    private String subTitle;
+
+}
