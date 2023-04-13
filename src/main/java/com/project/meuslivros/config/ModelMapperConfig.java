5700e9ffabe7ced9c505fc9e0f311eb977f8cfff
Dikendev <diken.dev@gmail.com>
2023-04-13 04:42:45 -0300

    ModelMapper config

---
+++ src/main/java/com/project/meuslivros/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.project.meuslivros.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+      return new ModelMapper();
+    }
+}
