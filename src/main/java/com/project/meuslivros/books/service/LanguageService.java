52fc94448e18ddf98d35eb2ccb93f63868a93ee1
Dikendev <diken.dev@gmail.com>
2023-04-13 04:41:40 -0300

    Language service using, NotFoundException.

---
+++ src/main/java/com/project/meuslivros/books/service/LanguageService.java
@@ -0,0 +1,42 @@
+package com.project.meuslivros.books.service;
+
+import com.project.meuslivros.books.entity.Language;
+import com.project.meuslivros.books.repository.LanguageRepository;
+import com.project.meuslivros.exception.NotFoundException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+
+@Service
+@AllArgsConstructor
+public class LanguageService {
+
+    private final LanguageRepository repository;
+
+    public Iterable<Language> findAllLanguage() {
+        return repository.findAll();
+    }
+
+    public Language findLanguageById(UUID id) {
+       return findOrThrow(id);
+    }
+
+    public Language addLanguage(Language language) {
+        return repository.save(language);
+    }
+
+    public void updateLanguage(UUID id, Language language) {
+        repository.save(language);
+    }
+
+    public Language findOrThrow(final UUID id) {
+        return repository
+                .findById(id)
+                .orElseThrow(
+                        () -> new NotFoundException("Language by id " +
+                                id + " was not found")
+                );
+    }
+}
