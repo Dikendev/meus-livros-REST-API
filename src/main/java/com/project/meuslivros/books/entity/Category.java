7448047f852f9ecdcfd5bc207ec4229ce131fcfb
Dikendev <diken.dev@gmail.com>
2023-04-13 04:44:07 -0300

    Category entity

--- src/main/java/com/project/meuslivros/books/entity/Category.java
+++ src/main/java/com/project/meuslivros/books/entity/Category.java
@@ -1,5 +1,6 @@
 package com.project.meuslivros.books.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -9,19 +10,21 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table
 @Data
+@Table(name = "categories")
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler","books"})
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO,
             generator = "UUID")
-    @Column(nullable = false, updatable = false)
+    @Column(name = "category_id" ,nullable = false, updatable = false)
     private UUID id;
 
     private String categoryName;
+
     @OneToMany(mappedBy = "category")
     private List<Book> books;
 }
