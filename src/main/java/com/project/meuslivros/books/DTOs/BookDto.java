b6261acf0a5d80915c8b4171413e7718454de89d
Dikendev <diken.dev@gmail.com>
2023-04-13 22:22:00 -0300

    redis mapping

--- src/main/java/com/project/meuslivros/books/DTOs/BookDto.java
+++ src/main/java/com/project/meuslivros/books/DTOs/BookDto.java
@@ -3,18 +3,20 @@ package com.project.meuslivros.books.DTOs;
 import jakarta.persistence.JoinColumn;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@RedisHash("Book")
 public class BookDto {
 
     private UUID Id;
     private UUID categoryId;
     @JoinColumn(name = "language_id")
-    private UUID LanguageId;
+    private UUID languageId;
 
     @NotBlank(message = "Title is required")
     private String title;
