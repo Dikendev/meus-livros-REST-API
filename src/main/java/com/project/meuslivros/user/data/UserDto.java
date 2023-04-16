2cc6607c6f3cf952bc8e5adb87466d9b02237e11
Dikendev <diken.dev@gmail.com>
2023-04-16 17:51:48 -0300

    User DTO

---
+++ src/main/java/com/project/meuslivros/user/data/UserDto.java
@@ -0,0 +1,20 @@
+package com.project.meuslivros.user.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private UUID id;
+
+    private String email;
+    private String mobileNumber;
+    private String password;
+
+}
