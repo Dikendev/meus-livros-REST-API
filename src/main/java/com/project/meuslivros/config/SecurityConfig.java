33641395865d12d0c42f4b30a8c09fabe28348d9
Dikendev <diken.dev@gmail.com>
2023-04-16 17:53:33 -0300

    Authentication manager configuration

--- src/main/java/com/project/meuslivros/config/SecurityConfig.java
+++ src/main/java/com/project/meuslivros/config/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.project.meuslivros.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
 }
