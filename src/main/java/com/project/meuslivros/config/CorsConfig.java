3086c5a9fce800fdd91b5b1284a1d16c1915f67e
Dikendev <diken.dev@gmail.com>
2023-04-15 23:22:16 -0300

    Allowed headers to send CORS request config

--- src/main/java/com/project/meuslivros/config/CorsConfig.java
+++ src/main/java/com/project/meuslivros/config/CorsConfig.java
@@ -16,7 +16,19 @@ public class CorsConfig {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
 
-
+        corsConfiguration.setAllowedHeaders(
+                Arrays.asList(
+                        "Origin",
+                        "Access-Control-Allow-Origin",
+                        "Content-Type",
+                        "Accept",
+                        "Authorization",
+                        "Origin", "Accept",
+                        "X-Requested-With",
+                        "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers"
+                )
+        );
 
     }
 }
