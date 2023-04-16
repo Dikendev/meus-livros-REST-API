ad67e4120dfddb6989598596888249862406dcf0
Dikendev <diken.dev@gmail.com>
2023-04-16 17:55:09 -0300

    User repository methods and query from DB

--- src/main/java/com/project/meuslivros/user/repository/UserRepository.java
+++ src/main/java/com/project/meuslivros/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.project.meuslivros.user.repository;
 
 import com.project.meuslivros.user.Entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -9,4 +10,14 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
+    @Query(
+        "" +
+            "SELECT CASE WHEN COUNT(u) 0 THEN " +
+            "TRUE ELSE FALSE END " +
+            "FROM UserEntity " +
+            "WHERE u.email = ?1"
+    )
+    Boolean selectExistByEmail(String email);
+
+    UserEntity findByEmail(String email);
 }
