621bf21545cce94ee38d2a21fc69b3def5fb0634
Dikendev <diken.dev@gmail.com>
2023-04-15 23:19:17 -0300

    Flag to allow credentials in cross-origin requests / Set allowed origins permitted to access the resources

--- src/main/java/com/project/meuslivros/config/CorsConfig.java
+++ src/main/java/com/project/meuslivros/config/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.filter.CorsFilter;
 
-
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -13,6 +13,9 @@ public class CorsConfig {
     @Bean
     CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+
 
 
     }
