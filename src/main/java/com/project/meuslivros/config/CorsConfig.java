5cdd96962a8386a1eae96fc4847c9e743d650efd
Dikendev <diken.dev@gmail.com>
2023-04-15 23:16:21 -0300

    CorsConfiguration object instantiated.

--- src/main/java/com/project/meuslivros/config/CorsConfig.java
+++ src/main/java/com/project/meuslivros/config/CorsConfig.java
@@ -1,9 +1,19 @@
 package com.project.meuslivros.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.CorsFilter;
+
 
 
 @Configuration
 public class CorsConfig {
 
+    @Bean
+    CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+
+    }
 }
