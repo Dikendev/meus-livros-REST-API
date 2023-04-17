99761f77f042b8ca23cfcadac8a2c44dab80ffff
Dikendev <diken.dev@gmail.com>
2023-04-16 23:24:29 -0300

    Post user method/end point

--- src/main/java/com/project/meuslivros/user/controller/UserController.java
+++ src/main/java/com/project/meuslivros/user/controller/UserController.java
@@ -2,10 +2,12 @@ package com.project.meuslivros.user.controller;
 
 import com.project.meuslivros.user.data.UserDto;
 import com.project.meuslivros.user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 
@@ -29,6 +31,13 @@ public class UserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUserById(@PathVariable("id") UUID id) {
         userService.removeUserById(id);
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto postUser(@Valid @RequestBody UserDto userDto)
+            throws NoSuchAlgorithmException {
+        return userService.createUser(userDto, userDto.getPassword());
     }
 
 }
