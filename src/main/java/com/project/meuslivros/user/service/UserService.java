1b98007b02782940f6b7e43a6b29f69336a5d577
Dikendev <diken.dev@gmail.com>
2023-04-16 23:02:56 -0300

    convert to Dto / Entity, error not found id

--- src/main/java/com/project/meuslivros/user/service/UserService.java
+++ src/main/java/com/project/meuslivros/user/service/UserService.java
@@ -1,9 +1,14 @@
 package com.project.meuslivros.user.service;
 
+import com.project.meuslivros.exception.NotFoundException;
+import com.project.meuslivros.user.entity.UserEntity;
+import com.project.meuslivros.user.data.UserDto;
 import com.project.meuslivros.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -11,5 +16,22 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper mapper;
+
+    private UserDto convertToDto(UserEntity entity) {
+        return mapper.map(entity, UserDto.class);
+    }
+
+    private UserEntity convertToEntity(UserDto dto) {
+        return mapper.map(dto, UserEntity.class);
+    }
+
+    private UserEntity findOrThrow(final UUID id) {
+      return userRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new NotFoundException("User by id" + id +
+                        " was not found")
+                );
+    }
 
 }
