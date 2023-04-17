59e0e3da47a522a84d5f45d50c10bf3f941d19a1
Dikendev <diken.dev@gmail.com>
2023-04-16 23:23:44 -0300

    Delete user method/end point

--- src/main/java/com/project/meuslivros/user/controller/UserController.java
+++ src/main/java/com/project/meuslivros/user/controller/UserController.java
@@ -2,12 +2,10 @@ package com.project.meuslivros.user.controller;
 
 import com.project.meuslivros.user.data.UserDto;
 import com.project.meuslivros.user.service.UserService;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 
@@ -25,6 +23,12 @@ public class UserController {
     @GetMapping("/api/v1/users/{id}")
     public UserDto getUserById(@PathVariable("id")UUID id) {
         return userService.findUserById(id);
+    }
+
+    @DeleteMapping("/api/v1/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUserById(@PathVariable("id") UUID id) {
+        userService.removeUserById(id);
     }
 
 }
