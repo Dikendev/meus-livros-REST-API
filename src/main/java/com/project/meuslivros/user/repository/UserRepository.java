a3e7f67f89c857fa64934d6914ff0077ceca0028
Dikendev <diken.dev@gmail.com>
2023-04-16 22:57:31 -0300

    User repository

--- src/main/java/com/project/meuslivros/user/repository/UserRepository.java
+++ src/main/java/com/project/meuslivros/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.project.meuslivros.user.repository;
 
-import com.project.meuslivros.user.Entity.UserEntity;
+import com.project.meuslivros.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,13 +11,12 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
     @Query(
-        "" +
-            "SELECT CASE WHEN COUNT(u) 0 THEN " +
-            "TRUE ELSE FALSE END " +
-            "FROM UserEntity " +
-            "WHERE u.email = ?1"
+            " " + "SELECT CASE WHEN COUNT(u) > 0 THEN " +
+                    "TRUE ELSE FALSE END " +
+                    "FROM UserEntity u " +
+                    "WHERE u.email = ?1"
     )
-    Boolean selectExistByEmail(String email);
+    Boolean selectExistsEmail(String email);
 
     UserEntity findByEmail(String email);
 }
