36c2f7cfcd71b5d2fc3e517a69f9bd73ac48472b
Dikendev <diken.dev@gmail.com>
2023-04-17 22:50:34 -0300

    encapsulate the user's email and password in a request for authentication

---
+++ src/main/java/com/project/meuslivros/jwt/models/AuthenticationRequest.java
@@ -0,0 +1,16 @@
+package com.project.meuslivros.jwt.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest implements Serializable {
+
+    private String email;
+    private String password;
+}
