6f8f7b1aed6580a8577089cbf60b7a57811b1f08
Dikendev <diken.dev@gmail.com>
2023-04-17 23:01:28 -0300

    method using HS256 symmetric key algorithm

--- src/main/java/com/project/meuslivros/jwt/util/JwtUtil.java
+++ src/main/java/com/project/meuslivros/jwt/util/JwtUtil.java
@@ -1,12 +1,23 @@
 package com.project.meuslivros.jwt.util;
 
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class JwtUtil {
 
     @Value("${jwt.secret}")
     private String SECRET_KEY;
+
+    private SecretKey getSecretKey() {
+        return new SecretKeySpec(
+                SECRET_KEY.getBytes(StandardCharsets.UTF_8),
+                SignatureAlgorithm.HS256.getJcaName());
+    }
 
 }
