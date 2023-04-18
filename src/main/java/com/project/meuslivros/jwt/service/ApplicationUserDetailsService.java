b188fe0f3487ea7587a1c5f964426b16e4d7615d
Dikendev <diken.dev@gmail.com>
2023-04-17 23:38:55 -0300

    userService instance to perform the authentication

--- src/main/java/com/project/meuslivros/jwt/service/ApplicationUserDetailsService.java
+++ src/main/java/com/project/meuslivros/jwt/service/ApplicationUserDetailsService.java
@@ -1,11 +1,23 @@
 package com.project.meuslivros.jwt.service;
 
+import com.project.meuslivros.jwt.models.UserPrincipal;
+import com.project.meuslivros.user.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class ApplicationUserDetailsService implements UserDetailsService {
 
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        return new UserPrincipal(userService.searchByEmail(email));
+    }
 }
