54b6d02cd3854b39024a36c4b7394edd624acbe0
Dikendev <diken.dev@gmail.com>
2023-04-17 23:35:07 -0300

    service for user authentication

---
+++ src/main/java/com/project/meuslivros/jwt/service/ApplicationUserDetailsService.java
@@ -0,0 +1,11 @@
+package com.project.meuslivros.jwt.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ApplicationUserDetailsService implements UserDetailsService {
+
+}
