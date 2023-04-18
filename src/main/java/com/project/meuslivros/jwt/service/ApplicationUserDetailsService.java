46e7b4353b33e6b91e9dbc9f0cacb5a6d964a3a8
Dikendev <diken.dev@gmail.com>
2023-04-17 23:57:20 -0300

    verify Password Hash method

--- src/main/java/com/project/meuslivros/jwt/service/ApplicationUserDetailsService.java
+++ src/main/java/com/project/meuslivros/jwt/service/ApplicationUserDetailsService.java
@@ -8,6 +8,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 @Service
 @AllArgsConstructor
 public class ApplicationUserDetailsService implements UserDetailsService {
@@ -16,8 +20,40 @@ public class ApplicationUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email)
-            throws UsernameNotFoundException {
+        throws UsernameNotFoundException {
 
         return new UserPrincipal(userService.searchByEmail(email));
+    }
+
+    private Boolean verifyPasswordHash(
+        String password,
+        Byte[] storedHash,
+        Byte[] storedSalt
+    ) throws NoSuchAlgorithmException {
+        if (password.isBlank() || password.isEmpty())
+            throw new IllegalArgumentException(
+                "Password cannot be empty or whitespace only string."
+        );
+
+        if (storedHash.length != 64)
+            throw new IllegalArgumentException(
+                "Invalid length of password hash(64 bytes expected)"
+        );
+
+        if (storedSalt.length != 128)
+            throw new IllegalArgumentException(
+                "Invalid length of password salt (64 bytes expected)"
+        );
+
+        var md = MessageDigest.getInstance("SHA-512");
+        md.update(storedSalt);
+
+        var computedHash = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        for (int i = 0; i < computedHash.length; i++) {
+            if (computedHash[i] != storedHash[i]) return false;
+        }
+
+        return MessageDigest.isEqual(computedHash, storedHash);
     }
 }
