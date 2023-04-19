7c4c8df39dbc41321066a5bb9efc715a954845a4
Dikendev <diken.dev@gmail.com>
2023-04-18 23:47:16 -0300

    custom authentication to controller

--- src/main/java/com/project/meuslivros/config/SecurityConfig.java
+++ src/main/java/com/project/meuslivros/config/SecurityConfig.java
@@ -1,21 +1,36 @@
 package com.project.meuslivros.config;
 
+import com.project.meuslivros.jwt.filters.JwtRequestFilter;
+import com.project.meuslivros.jwt.service.ApplicationUserDetailsService;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
+    private final ApplicationUserDetailsService userDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    protected void configure(AuthenticationManagerBuilder auth)
+        throws Exception {
+            auth.userDetailsService(userDetailsService);
+    }
 
     @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
     public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -38,7 +53,9 @@ public class SecurityConfig {
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                return http.build();
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 
 }
