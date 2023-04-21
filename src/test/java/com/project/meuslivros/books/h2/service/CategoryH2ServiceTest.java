ebda13edacd0b8d970eb349b4b8490bc356d09fd
Dikendev <diken.dev@gmail.com>
2023-04-20 23:57:04 -0300

    injecting dependency

--- src/test/java/com/project/meuslivros/books/h2/service/CategoryH2ServiceTest.java
+++ src/test/java/com/project/meuslivros/books/h2/service/CategoryH2ServiceTest.java
@@ -1,10 +1,16 @@
 package com.project.meuslivros.books.h2.service;
 
+import com.project.meuslivros.books.repository.CategoryRepository;
+import com.project.meuslivros.books.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 
 @DataJpaTest
 public class CategoryH2ServiceTest {
 
+    @Autowired
+    private CategoryRepository repository;
+    private CategoryService service;
 
 }
