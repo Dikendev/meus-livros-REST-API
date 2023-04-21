acb6538c4529a4a0a36f7a7837d93d3c89cb0f97
Dikendev <diken.dev@gmail.com>
2023-04-20 23:53:57 -0300

    JUnit and AssertJ

---
+++ src/test/java/com/project/meuslivros/books/h2/service/CategoryH2ServiceTest.java
@@ -0,0 +1,10 @@
+package com.project.meuslivros.books.h2.service;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+
+@DataJpaTest
+public class CategoryH2ServiceTest {
+
+
+}
