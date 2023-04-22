b9c98d8603a3dfdb1120107cc358b34a7b76db9c
Dikendev <diken.dev@gmail.com>
2023-04-22 02:18:35 -0300

    Mockito JUnit test objects

---
+++ src/test/java/com/project/meuslivros/books/service/CategoryServiceTest.java
@@ -0,0 +1,8 @@
+package com.project.meuslivros.books.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CategoryServiceTest {
+}
