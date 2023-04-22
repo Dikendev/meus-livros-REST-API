bd411097b328499730d48912e001457637285ab4
Dikendev <diken.dev@gmail.com>
2023-04-22 08:42:56 -0300

    Mockito JUnit to add category

--- src/test/java/com/project/meuslivros/books/service/CategoryServiceTest.java
+++ src/test/java/com/project/meuslivros/books/service/CategoryServiceTest.java
@@ -1,13 +1,18 @@
 package com.project.meuslivros.books.service;
 
+import com.project.meuslivros.books.entity.Category;
 import com.project.meuslivros.books.repository.CategoryRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+
 
 @ExtendWith(MockitoExtension.class)
 public class CategoryServiceTest {
@@ -24,5 +29,21 @@ public class CategoryServiceTest {
 
         verify(repository).findAll();
     }
+
+    @Test
+    void canAddCategory() {
+        Category category = new Category("Diego", null);
+
+        serviceTest.addCategory(category);
+
+        ArgumentCaptor<Category> categoryArgumentCaptor = ArgumentCaptor.forClass(Category.class);
+
+        verify(repository).save(categoryArgumentCaptor.capture());
+        Category captureCategory = categoryArgumentCaptor.getValue();
+
+        assertThat(captureCategory).isEqualTo(category);
+    }
+
+
 
 }
