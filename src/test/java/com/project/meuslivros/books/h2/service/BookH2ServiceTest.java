3fc0483bc2c50d59e2564512ed471227819703a1
Dikendev <diken.dev@gmail.com>
2023-04-22 01:42:21 -0300

    Setup for Junit/AssertJ Book tests

--- src/test/java/com/project/meuslivros/books/h2/service/BookH2ServiceTest.java
+++ src/test/java/com/project/meuslivros/books/h2/service/BookH2ServiceTest.java
@@ -7,6 +7,7 @@ import com.project.meuslivros.books.repository.BookRepository;
 import com.project.meuslivros.books.repository.CategoryRepository;
 import com.project.meuslivros.books.repository.LanguageRepository;
 import com.project.meuslivros.books.service.BookService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -27,5 +28,20 @@ public class BookH2ServiceTest {
     Category category = new Category();
     Language language = new Language();
 
+    @BeforeEach
+    public void setup() {
+        language.setLanguageName("English");
+        category.setCategoryName("oo");
+
+        languageRepository.save(language);
+        categoryRepository.save(category);
+
+        book.setTitle("O Iluminado");
+        book.setSubTitle("REDRUM");
+        book.setCategory(category);
+        book.setLanguage(language);
+
+        service = new BookService(repository);
+    }
 
 }
