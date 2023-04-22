b86252691df04067bdb712be6341aa4006c12b3d
Dikendev <diken.dev@gmail.com>
2023-04-21 23:12:08 -0300

    instance of repository and service

--- src/test/java/com/project/meuslivros/books/h2/service/LanguageH2ServiceTest.java
+++ src/test/java/com/project/meuslivros/books/h2/service/LanguageH2ServiceTest.java
@@ -1,10 +1,17 @@
 package com.project.meuslivros.books.h2.service;
 
+import com.project.meuslivros.books.repository.LanguageRepository;
+import com.project.meuslivros.books.service.LanguageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 
 @DataJpaTest
 public class LanguageH2ServiceTest {
+
+    @Autowired
+    private LanguageRepository repository;
+    private LanguageService service;
 
 
 }
