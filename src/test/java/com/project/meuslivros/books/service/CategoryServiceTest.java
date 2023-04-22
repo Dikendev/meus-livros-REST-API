623288ad965171278c4397d4ed30dd0350071bde
Dikendev <diken.dev@gmail.com>
2023-04-22 02:27:15 -0300

    injecting Category mock objects

--- src/test/java/com/project/meuslivros/books/service/CategoryServiceTest.java
+++ src/test/java/com/project/meuslivros/books/service/CategoryServiceTest.java
@@ -1,8 +1,20 @@
 package com.project.meuslivros.books.service;
 
+import com.project.meuslivros.books.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class CategoryServiceTest {
+
+    @Mock
+    private CategoryRepository repository;
+    @InjectMocks
+    private CategoryService serviceTest;
+
 }
