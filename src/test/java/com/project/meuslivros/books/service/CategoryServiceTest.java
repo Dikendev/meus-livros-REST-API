e75ee73b9b2249cf4e2077e490d6a180595258d2
Dikendev <diken.dev@gmail.com>
2023-04-22 22:08:10 -0300

    Mockito tests Setup and time(1) to check if method is called at least 1 time.

--- src/test/java/com/project/meuslivros/books/service/CategoryServiceTest.java
+++ src/test/java/com/project/meuslivros/books/service/CategoryServiceTest.java
@@ -2,6 +2,7 @@ package com.project.meuslivros.books.service;
 
 import com.project.meuslivros.books.entity.Category;
 import com.project.meuslivros.books.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -9,9 +10,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -22,23 +25,30 @@ public class CategoryServiceTest {
     @InjectMocks
     private CategoryService serviceTest;
 
+    Category category = new Category();
+
+    @BeforeEach
+    void setup() {
+        category.setCategoryName("Diego");
+        category.setBooks(null);
+        category.setId(UUID.randomUUID());
+    }
+
     @Test
     void canFindAllCategories() {
 
         serviceTest.findAllCategory();
 
-        verify(repository).findAll();
+        verify(repository,times(1)).findAll();
     }
 
     @Test
     void canAddCategory() {
-        Category category = new Category("Diego", null);
-
         serviceTest.addCategory(category);
 
         ArgumentCaptor<Category> categoryArgumentCaptor = ArgumentCaptor.forClass(Category.class);
 
-        verify(repository).save(categoryArgumentCaptor.capture());
+        verify(repository, times(1)).save(categoryArgumentCaptor.capture());
         Category captureCategory = categoryArgumentCaptor.getValue();
 
         assertThat(captureCategory).isEqualTo(category);
